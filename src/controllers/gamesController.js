2321199719b7ed47aa6bc73683b4ddeb3d95afd6
MatheusMachad00 <matheus_m_machado@hotmail.com>
2022-07-22 11:20:55 -0300

    Feat: Route get shows the category of games

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -1,21 +1,46 @@
 import connection from "../dbStrategy/postgres.js";
 
-export async function getGames(_, res) {
+export async function getGames(req, res) {
   try {
-    const { rows: games } = await connection.query('SELECT * FROM games');
-    res.send(games);
+
+      const { rows: games } = await connection.query(`
+      SELECT games.*, categories.name AS "categoryName"
+      FROM games
+      JOIN categories
+      ON games."categoryId" = categories.id;`);
+
+      res.send(games);
   } catch (error) {
     res.sendStatus(500);
     console.error(error);
   }
 }
 
+/* export async function getGamesByName(req, res) {
+  try {
+    //const { name } = req.params;
+    let { name } = req.query;
+
+    const { rows: games } = await connection.query(`
+    SELECT * FROM games WHERE to_tsvector(name) @@ to_tsquery('$1:*')
+    `, [name]);
+
+    res.send(games);
+  } catch (error) {
+    res.sendStatus(500);
+    console.error(error);
+  }
+} */
+
 export async function postGame(req, res) {
   try {
     const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
 
     await connection.query(
-      'INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1,$2,$3,$4,$5)',
+      `
+      INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") 
+      VALUES ($1,$2,$3,$4,$5);
+      `,
       [name, image, stockTotal, categoryId, pricePerDay]
     );
 
